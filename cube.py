41238c2481dbbde3bd5ca62dcfe1ed889d8d48db
jan <john.vicha@seznam.cz>
2018-10-17 23:13:59 +0200

    Initial commit

---
+++ cube.py
@@ -0,0 +1,50 @@
+name = input("Hi, whats your name? ")
+length = len(name)
+print("Hello {0}! Yours name has {1} symbol".format(name,length))
+
+
+number = int(input("Enter the number: "))
+print("In binary: {0:0>4b}, last value at the number is {0}, ".format(int(number/10*10)))
+
+def cube(value):
+    cube = str("""
+    +-------+
+    | {}   {} |
+    | {} {} {} |
+    | {}   {} |
+    +-------+
+    \n
+    """)
+    c1 = ("Number 1" + cube.format(" ", " ", " ", "o", " ", " ", " "))
+    c2 = ("Number 2" + cube.format(" ", "o", " ", " ", " ", "o", " "))
+    c3 = ("Number 3" + cube.format(" ", "o", " ", "o", " ", "o", " "))
+    c4 = ("Number 4" + cube.format("o", "o", " ", " ", " ", "o", "o"))
+    c5 = ("Number 5" + cube.format("o", "o", " ", "o", " ", "o", "o"))
+    c6 = ("Number 6" + cube.format("o", "o", "o", " ", "o", "o", "o"))
+    wrong = ("Wrong enter")
+    error = ("Error enter")
+
+    value = int(value)
+    try:
+        if value == 1:
+            print(c1)
+        elif value == 2:
+            print(c2)
+        elif value == 3:
+            print(c3)
+        elif value == 4:
+            print(c4)
+        elif value == 5:
+            print(c5)
+        elif value == 6:
+            print(c6)
+        elif value == 7:
+            print(c1 + c2 +c3 + c4 + c5 + c6)
+        else:
+            print(wrong)
+    except:
+        print(error)
+    return
+
+value = input("Enter the number of cube (1-6) you want to see, or 7 to see all: ")
+value = (cube(value))
