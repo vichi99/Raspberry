d538529a8326cb18249b95ce04bee1772aa928fc
jan <john.vicha@seznam.cz>
2018-10-25 16:47:59 +0200

    something

--- cube.py
+++ cube.py
@@ -1,18 +1,31 @@
 #by Jan Vicha
-name = input("Hi, whats your name? ")
-length = len(name)
-print("Hello {0}! Yours name has {1} symbol".format(name,length))
+def name():
+    name = input("Hi, whats your name? ")
+    length = len(name)
+    print("Hello {0}! Yours name has {1} symbol".format(name,length))
 
-cond = True
-while cond == True:
-    try:
-        number = int(input("Enter the int number: "))
-        print("The number {0} is in binary: {0:0>4b} and the last value is {1} ".format(number,int(number % 10)))
-        cond = False
-    except:
-        print("The input is wrong. Type number")
+def number():
+    cond = True
+    while cond == True:
+        try:
+            text = str("The number {0} is in binary: {0:0>4b} and the last value is {1}")
+            number = int(input("Enter the int number: "))
+            length = len(str(number))
+            if length == 1:
+                print(text.format(number,int(number % 10)))
+            if length == 2:
+                print((text + ", {2}").format(number,int(number % 10),int(number % 100 / 10 )))
+            if length >= 3:
+                print((text + ", {2}, {3}").format(number,int(number % 10),int(number % 100 / 10 ),int(number % 1000 / 100)))
+            else:
+                pass
+            cond = False
+        except:
+            print("The input is wrong. Type number")
+    return
 
-def cube(value):
+def cube():
+    value = input("Enter the number of cube (1-6) you want to see, or 7 to see all: ")
     cube = str("""
     +-------+
     | {}   {} |
@@ -52,5 +65,6 @@ def cube(value):
         print(error)
     return
 
-value = input("Enter the number of cube (1-6) you want to see, or 7 to see all: ")
-value = (cube(value))
+name()
+number()
+cube()
